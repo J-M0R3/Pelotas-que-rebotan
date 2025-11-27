2fd1099f4be86df92bd67f7e5db3396156b42ab1
J.More <juan.morejonu@ucuenca.edu.ec>
2025-11-27 16:56:19 -0500

    BUrbujas

--- classes/Pelota.js
+++ classes/Pelota.js
@@ -1,44 +1,80 @@
 class Pelota {
-         constructor () {
+    constructor() {
 
-         this.diam = random (30, 150);
-         this.rad = this.diam / 2;
+        this.diam = random(40, 120);
+        this.rad = this.diam / 2;
 
-         this.posx = random (this.rad, width - this.rad);
-         this.posy = random (this.rad, height - this.rad);
+        this.posx = random(this.rad, width - this.rad);
+        this.posy = random(this.rad, height - this.rad);
 
-         this.velx = random (-5, 5);
-         this.vely = random (-5, 5);
+        this.velx = random(-3, 3);
+        this.vely = random(-3, 3);
 
-         this.nuevocolor = color (random (255), random (255), random (50 ,175) );
+        this.baseColor = color(random(255), random(255), random(100, 200));
 
-         }
+        // Tipo de diseño:
+        // 0 = esfera 3D realista
+        // 1 = burbuja de cristal
+        this.tipo = random([0, 1]);
+    }
 
+    actualizar() {
+        if (this.posx > width - this.rad || this.posx < this.rad) {
+            this.velx *= -1;
+        }
 
-         actualizar() {
-             if (this.posx > width - this.rad || this.posx < this.rad) {
-         this.velx *= -1;
-            }
+        if (this.posy > height - this.rad || this.posy < this.rad) {
+            this.vely *= -1;
+        }
 
-         if (this.posy > height - this.rad || this.posy < this.rad) {
-         this.vely *= -1;
-            }
+        this.posx += this.velx;
+        this.posy += this.vely;
+    }
 
+    visualizar() {
 
-         this.posx += this.velx;
-         this.posy += this.vely;
+        if (this.tipo === 0) {
+            this.dibujarEsfera3D();
+        } else {
+            this.dibujarBurbujaCristal();
+        }
+    }
 
-            }
- 
-         visualizar(){
-         fill (this.nuevocolor);
-         circle(this.posx, this.posy, this.diam);
-         stroke ("#e7a916ff");
-         strokeWeight(10);
- 
-         fill ("#e7a916ff");
-         ellipse(this.posx, this.posy, this.diam / 2, this.diam / 9);
- 
-         }   
+    // 🌟 1. ESFERA 3D REALISTA  
+    dibujarEsfera3D() {
 
- }       
+        // Círculo base
+        noStroke();
+        fill(this.baseColor);
+        circle(this.posx, this.posy, this.diam);
+
+        // Luz principal (gradiente simulado)
+        fill(255, 255, 255, 90);
+        circle(this.posx - this.rad * 0.3, this.posy - this.rad * 0.3, this.diam * 0.45);
+
+        // Sombra inferior
+        fill(0, 0, 0, 80);
+        circle(this.posx + this.rad * 0.25, this.posy + this.rad * 0.25, this.diam * 0.7);
+    }
+
+    // ✨ 2. BURBUJA DE CRISTAL  
+    dibujarBurbujaCristal() {
+
+        // Capa principal transparente
+        noStroke();
+        fill(red(this.baseColor), green(this.baseColor), blue(this.baseColor), 60);
+        circle(this.posx, this.posy, this.diam);
+
+        // Halo brillante (glow suave)
+        fill(255, 255, 255, 40);
+        circle(this.posx, this.posy, this.diam * 1.15);
+
+        // Reflejo fuerte
+        fill(255, 255, 255, 150);
+        ellipse(this.posx - this.rad * 0.3, this.posy - this.rad * 0.35, this.diam * 0.35, this.diam * 0.22);
+
+        // Reflejo pequeño
+        fill(255, 255, 255, 220);
+        ellipse(this.posx + this.rad * 0.25, this.posy + this.rad * 0.15, this.diam * 0.12, this.diam * 0.08);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,33 +1,24 @@
- let nuevaPelota; 
-
- const NP = 200;
-
+const NP = 400;
 let pelotas = [];
 
-
- function setup() {
+function setup() {
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER);
-    
 
-for (let i = 0; i < NP; i++) {
-    let nuevaPelota = new Pelota();
-    pelotas[i] = nuevaPelota;
- }   
-  }  
-
-
- function draw() {
-    background(150);
-    
     for (let i = 0; i < NP; i++) {
-    pelotas[i].actualizar();
-    pelotas[i].visualizar();
- }  
+        pelotas[i] = new Pelota();
+    }
+}
 
+function draw() {
+    background(20); // fondo más elegante
 
-    
- }
+    for (let i = 0; i < NP; i++) {
+        pelotas[i].actualizar();
+        pelotas[i].visualizar();
+    }
+}
+
  
 
        
